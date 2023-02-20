826d7f4820b5cc78e5fc73758104059e231dab78
Tasreen Mowreen <tmowreen97@gmail.com>
2023-02-20 13:30:39 -0500

    commit tree 80d6a9d866d20c563d3f43fd0df94c82309399b8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,18 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
-    render json: students
+    render json: students, status: :ok
   end
 
   def grades
-    students = Student.all.order(grade: :desc)
-    render json: students
+    students = Student.all.sort_by{|student| student[:grade]}.reverse!
+    render json: students, status: :ok
   end
 
   def highest_grade
-    smartest = Student.order(grade: :desc).first
-    render json: smartest
+    students = Student.all.sort_by{|student| student[:grade]}.reverse!
+    render json: students.first, status: :ok
   end
+
 
 end
